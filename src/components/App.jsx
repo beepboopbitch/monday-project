d1bb2858291c734d9c7960c969674ce9bf904fba
Drew Yoxsimer and Nayomi Morita <drew+nayomi@>
2019-04-22 10:26:05 -0700

    add all

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Header from './Header';
+import TicketList from './TicketList';
+import { Switch, Route } from 'react-router-dom';
+import NewTicketForm from './NewTicketForm';
+import NewTicketControl from './NewTicketControl';
+
+function App(){
+  return(
+    <div>
+      <Header/>
+      <Switch>
+        <Route exact path='/' component={TicketList} />
+        <Route path='/newticket' component={NewTicketControl} />
+      </Switch>
+    </div>
+
+  );
+}
+
+export default App;
